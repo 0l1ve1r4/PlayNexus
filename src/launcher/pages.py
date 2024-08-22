3f2730c6fcab9f9b8ba3784c69a6626f37cf9abb
Arthur Ferreira <arthur.sfc@aluno.ufop.edu.br>
2024-08-22 12:00:13 -0300

    Added sign up form

--- src/launcher/pages.py
+++ src/launcher/pages.py
@@ -3,6 +3,8 @@ import customtkinter as ctk
 from PIL import Image
 from .utils import *
 from .backend.backend import *
+from functools import partial
+
 
 SIDE_BAR_COLOR = "#2a2a2a"
 
@@ -22,7 +24,7 @@ class Pages:
             content_frame = ctk.CTkFrame(master=home_frame, fg_color="transparent")
             content_frame.pack(anchor="n", fill="both", padx=24, pady=24)
 
-            self.create_search_bar(content_frame)
+            self.create_search_bar(content_frame, " Search in store")
             self.create_labels_and_content(content_frame,"Recently added")
         else:
             self.frames["home_page"].pack(fill="both", expand=True)
@@ -37,23 +39,26 @@ class Pages:
             content_frame = ctk.CTkFrame(master=library_frame, fg_color="transparent")
             content_frame.pack(anchor="n", fill="x", padx=24, pady=24)
 
-            self.create_search_bar(content_frame)
-            #self.create_labels_and_content(content_frame,"Last played")
+            self.create_search_bar(content_frame, " Search in library")
+            self.create_labels_and_content(content_frame,"Last played")
         else:
             self.frames["library_page"].pack(fill="both", expand=True)
 
-    def create_search_bar(self, master: ctk.CTkFrame) -> None:
+    def create_search_bar(self, master: ctk.CTkFrame, placeholder) -> None:
         """Create the first line of the main view with a search bar and a button."""
         first_line = ctk.CTkFrame(master=master, fg_color="transparent")
         first_line.pack(anchor="n", fill="x")
 
-        search_entry = ctk.CTkEntry(master=first_line, width=400, height=30,
-                                    font=("Roboto", 12), placeholder_text="Search in store")
-        search_entry.pack(side="left")
+        search_entry = ctk.CTkEntry(master=first_line, height=30,
+                                    font=("Roboto", 12), placeholder_text=placeholder)
+        search_entry.pack(side="left", fill="x", expand=True)
+
+        icon = load_image("search-md.png")
+        icon = ctk.CTkImage(dark_image=icon, light_image=icon, size=(16, 16))
 
         search_button = ctk.CTkButton(master=first_line, height=30, text="Search", text_color="#ffffff",
-                                      width=225, command=search_in_store)
-        search_button.pack(anchor="w", padx=(8, 0))
+                                      width=112, command=search_in_store, compound="left", image=icon)
+        search_button.pack(anchor="w", padx=(8, 0), side="right")
 
     def create_labels_and_content(self, master: ctk.CTkFrame, header) -> None:
         """Create labels and content sections."""
@@ -104,3 +109,5 @@ class Pages:
         self.main_view = ctk.CTkFrame(master=self.app, width=680, height=650, corner_radius=0)
         self.main_view.pack_propagate(0)
         self.main_view.pack(side="left")
+
+        self.show_frame(self.home_page)
--- src/login/signup.py
+++ src/login/signup.py
@@ -3,16 +3,18 @@ import os
 from PIL import Image
 from typing import Tuple
 
+from login import Login
+
 ctk.set_default_color_theme("res/themes/purple.json")
 
-class Login:
+class Signup:
     def __init__(self) -> None:
         """Initialize the login window and load necessary resources."""
         self.res_path = 'res/'
         self.app = ctk.CTk()
-        self.app.geometry("322x480")
+        self.app.geometry("322x600")
         self.app.resizable(False, False)
-        self.app.title("PlayNexus | Login")
+        self.app.title("PlayNexus | Sign up")
         
         if os.name == "nt":
             icon_path = os.path.join(self.res_path, 'secondary-logo-colored.ico')
@@ -22,9 +24,11 @@ class Login:
                 print(f"Icon file not found at {icon_path}")
 
         # Load images
+        self.name_icon = self.load_image("type-square.png", (16, 16))
         self.login_logo = self.load_image("secondary-logo-white.png", (42, 38))
         self.email_icon = self.load_image("email-icon.png", (16, 16))
         self.password_icon = self.load_image("password-icon.png", (16, 16))
+        self.birth_icon = self.load_image("calendar.png", (16, 16))
         self.google_icon = self.load_image("google-icon.png", (16, 16))
 
         # Initialize variables
@@ -53,17 +57,22 @@ class Login:
         if self.login_logo:
             ctk.CTkLabel(master=frame, text="", image=self.login_logo).pack(anchor="nw", padx=(24, 0), pady=(24, 0))
 
-        # Welcome and instructions
-        ctk.CTkLabel(master=frame, text="Welcome back!", anchor="w",
+        # Header
+        ctk.CTkLabel(master=frame, text="Create a new account", anchor="w",
                      justify="left", font=("Inter", 24)).pack(anchor="w", pady=(8, 0), padx=(24, 24))
-        ctk.CTkLabel(master=frame, text="Sign in to your account", anchor="w",
-                     justify="left").pack(anchor="w", pady=(0, 0), padx=(24, 24))
+
+        # Full name entry
+        if self.email_icon:
+            ctk.CTkLabel(master=frame, text="  Full name", anchor="w", justify="left",
+                         image=self.name_icon, compound="left").pack(anchor="w", pady=(16, 0), padx=(24, 24))
+        self.email_entry = ctk.CTkEntry(master=frame, border_width=2, placeholder_text="Enter your full name")
+        self.email_entry.pack(anchor="w", padx=(24, 24), fill="x")
 
         # Email entry
         if self.email_icon:
             ctk.CTkLabel(master=frame, text="  Email", anchor="w", justify="left",
                          image=self.email_icon, compound="left").pack(anchor="w", pady=(16, 0), padx=(24, 24))
-        self.email_entry = ctk.CTkEntry(master=frame, border_width=2, placeholder_text="Enter your email")
+        self.email_entry = ctk.CTkEntry(master=frame, border_width=2, placeholder_text="Ex: playnexus@youremail.com")
         self.email_entry.pack(anchor="w", padx=(24, 24), fill="x")
 
         # Password entry
@@ -73,6 +82,20 @@ class Login:
         self.passw_entry = ctk.CTkEntry(master=frame, border_width=2, show="*", placeholder_text="Enter your password")
         self.passw_entry.pack(anchor="w", padx=(24, 24), fill="x")
 
+        # Confirm password entry
+        if self.password_icon:
+            ctk.CTkLabel(master=frame, text="  Confirm Password*", anchor="w", justify="left",
+                         font=("Roboto Medium", 12), image=self.password_icon, compound="left").pack(anchor="w", pady=(16, 0), padx=(24, 0))
+        self.passw_entry = ctk.CTkEntry(master=frame, border_width=2, show="*", placeholder_text="Please, confirm your password")
+        self.passw_entry.pack(anchor="w", padx=(24, 24), fill="x")
+
+        # Birthdate entry
+        if self.birth_icon:
+            ctk.CTkLabel(master=frame, text="  Enter your birth date*", anchor="w", justify="left",
+                         font=("Roboto Medium", 12), image=self.password_icon, compound="left").pack(anchor="w", pady=(16, 0), padx=(24, 0))
+        self.birth_entry = ctk.CTkEntry(master=frame, border_width=2, show="*", placeholder_text="DD/MM/YY")
+        self.birth_entry.pack(anchor="w", padx=(24, 24), fill="x")
+
         # Error message label
         self.error_label = ctk.CTkLabel(master=frame, text="Wrong email or password", text_color="#FF0000",
                                         anchor="w", justify="left")
@@ -80,55 +103,29 @@ class Login:
         self.error_label.pack_forget()  # Hide initially
 
         # Login button
-        login_button = ctk.CTkButton(master=frame, text="Login", command=self.check_credentials)
-        login_button.pack(anchor="w", pady=(32, 8), padx=(24, 24), fill="x")
+        content_frame = ctk.CTkFrame(master=frame, fg_color="transparent")
+        content_frame.pack(anchor="w", fill="x", pady=(32, 8), padx=(24, 24))
 
-        # Signup and Google buttons
-        ctk.CTkButton(master=frame, text="Sign up", fg_color="transparent", hover_color="#4d4d4d",
-                      font=("Roboto Medium", 12), text_color="#fff", border_width=2, border_color="#b3b3b3").pack(anchor="w", pady=(0, 0), padx=(24, 24), fill="x")
-        
-        ctk.CTkLabel(master=frame, text_color="#2a2a2a",
-                     text="_______________________________________________________",
-                     fg_color="transparent").pack(fill="x", pady=(0, 0))
-        
-        ctk.CTkButton(master=frame, text="Continue With Google", fg_color="#fff", hover_color="#fff",
-                      font=("Roboto Medium", 12), text_color="#000", image=self.google_icon).pack(anchor="w", pady=(20, 0), padx=(24, 24), fill="x")
+        cancel_button = ctk.CTkButton(master=content_frame, text="Cancel", fg_color="transparent", hover_color="#4d4d4d",
+                                      border_width=2, border_color="#b3b3b3", width=132, command=self.return_to_previous_page)
+        cancel_button.pack(anchor="w", side="left", padx=(0, 8))
+        login_button = ctk.CTkButton(master=content_frame, text="Create account", width=132)
+        login_button.pack(anchor="w", side="right")
 
-        self.app.bind("<Return>", self.handle_enter)
+        agreements_frame = ctk.CTkFrame(master=frame, fg_color="transparent")
+        agreements_frame.pack(anchor="w", fill="x", padx=(24, 24))
 
-    def handle_enter(self, event: str) -> None:
-        """Handle the Enter key press event."""
-        print(f"{event} pressed")
-        self.check_credentials()
+        agreements_label = ctk.CTkLabel(master=agreements_frame, text="By creating a PlayNexus account you’re agreeing with our Privacy Policy and accept our Terms and Conditions.",
+                                        anchor="w", justify="left", wraplength=274, font=ctk.CTkFont(family='Roboto', size=12), text_color="#b3b3b3")
+        agreements_label.pack(anchor="w", fill="x")
 
-    def check_credentials(self) -> None:
-        """Check the credentials and display an error if necessary."""
-        email = self.email_entry.get()
-        password = self.passw_entry.get()
+        self.app.bind("<Return>")
 
-        wrong_credentials = not self.authenticate_user(email, password)
-
-        if wrong_credentials and self.missed_attempts == 0:
-            self.missed_attempts += 1
-            return
-
-        if wrong_credentials:
-            self.error_label.pack()
-        else:
-            self.error_label.pack_forget()
-            self.break_loop()
-
-    def authenticate_user(self, email: str, password: str) -> bool:
-        """Return true if credentials are valid, false otherwise."""
-        print(f"Email: {email}, Password: {password}")
-        self.is_logged_in = True
-
-        return email == "admin" and password == "admin"
-
-    def break_loop(self) -> None:
-        """Close the application if login is successful."""
+    def return_to_previous_page(self):
         self.app.destroy()
+        login = Login()
+        login.app.mainloop()
 
 if __name__ == "__main__":
-    login_app = Login()
-    login_app.app.mainloop()
+    signup_app = Signup()
+    signup_app.app.mainloop()
