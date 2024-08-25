35bf724b3594cd686403166697e6415e6765f329
Franklin L. Tomich <134819642+frantomich@users.noreply.github.com>
2024-08-25 14:35:41 -0300

    Created the database structure definition script.

---
+++ src/back/create.sql
@@ -0,0 +1,62 @@
+-- PlayNexus application database structure definition script.
+-- WARNING: Running this will erase and rebuild the database!
+-- Requires a MySQL database server to run.
+
+DROP DATABASE IF EXISTS PlayNexus;
+
+CREATE DATABASE PlayNexus;
+
+USE PlayNexus;
+
+-- Creates the table that will store user's account information:
+CREATE TABLE Account (
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (email)
+);
+
+-- Creates the table that will store the gamer's information:
+CREATE TABLE Gamer (
+    account VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    birth_date DATE NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    bio TEXT NOT NULL,
+    PRIMARY KEY (account),
+    FOREIGN KEY (account) REFERENCES Account(email) ON DELETE CASCADE
+);
+
+-- Creates the table that will store the publisher's information:
+CREATE TABLE Publisher (
+    account VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (account),
+    FOREIGN KEY (account) REFERENCES Account(email) ON DELETE CASCADE
+);
+
+-- Creates the table that will store the game's information:
+CREATE TABLE Game (
+    title VARCHAR(255) NOT NULL,
+    publisher VARCHAR(255) NOT NULL,
+    developer VARCHAR(255) NOT NULL,
+    genre VARCHAR(255) NOT NULL,
+    publication_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    description TEXT NOT NULL,
+    logo BLOB NOT NULL,
+    installer BLOB NOT NULL,
+    price DECIMAL(5, 2) NOT NULL,
+    PRIMARY KEY (title, publisher),
+    FOREIGN KEY (publisher) REFERENCES Publisher(account) ON DELETE CASCADE
+);
+
+-- Creates the table that will store the buy's information:
+CREATE TABLE Buy (
+    gamer VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    publisher VARCHAR(255) NOT NULL,
+    purchase_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (gamer, title, publisher),
+    FOREIGN KEY (gamer) REFERENCES Gamer(account) ON DELETE CASCADE,
+    FOREIGN KEY (title, publisher) REFERENCES Game(title, publisher) ON DELETE CASCADE
+);
